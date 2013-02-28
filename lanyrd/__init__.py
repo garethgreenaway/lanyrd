265df5ab28f15d54d12762b9d9dbafc3fd5fa3c7
Gareth J. Greenaway <gareth@wiked.org>
2013-02-28 12:13:32 -0800

    initial check in.

---
+++ lanyrd/__init__.py
@@ -0,0 +1,54 @@
+import json
+import urllib2
+
+class Lanyrd(object):
+  
+  def __init__(self):
+    pass  
+
+  def search(self, query):   
+    results = []
+    for row in self._get("search/?q=%s" % (query))['sections']:
+      results = results + row['rows']
+    return results    
+    
+  def popular(self):
+    results = []
+    for row in self._get("search/")['sections']:
+      results = results + row['rows']
+    return results
+
+  def event(self, slug, year):
+    return self._get("%s/%s/" % (year, slug))
+
+  def speakers(self, slug, year):
+    results = []
+    for row in self._get("%s/%s/speakers/" % (year,slug))['sections']:
+      results = results + row['rows']
+    return results
+
+  def attendees(self, slug, year):
+    results = []
+    for row in self._get("%s/%s/attendees/" % (year,slug))['sections']:
+      results = results + row['rows']
+    return results
+    
+  def schedule(self, slug, year):
+    results = []
+    for row in self._get("%s/%s/schedule/" % (year,slug))['sections']:
+      results = results + row['rows']
+    return results
+
+  def profile(self, username):
+    return self._get("profile/%s/" % (username))
+
+  def future_events(self, username):
+    return self._get("profile/%s/action/" % (username))['events']
+
+  def _get(self, path):
+    url = 'http://lanyrd.com/mobile/ios2/' + path
+    response = urllib2.urlopen(url)
+    data = response.read()
+    json_data = json.loads(data)
+    response.close()
+    return json_data
